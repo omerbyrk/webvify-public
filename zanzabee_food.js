bd2abab7226da2158ab0e9e7ac0bcf61829ab6a1
Ömer Bayrak <omersocial8@gmail.com>
2022-07-27 12:28:47 +0300

    Create zanzabee_food.js

---
+++ zanzabee_food.js
@@ -0,0 +1,13 @@
+
+
+function hideItemsRepeater(callback) {
+    var interval = setInterval(callback, 50);
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 500);
+}
+
+    try {
+    document.getElementById("T1514610365328").style.display = "none";
+    hideItemsRepeater(() => document.getElementById("T1514610365328").style.display = "none");
+    } catch { }
