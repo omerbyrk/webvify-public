d61efee47dc22b6bc9137f23556bf08be37442bc
Ömer Bayrak <omersocial8@gmail.com>
2022-10-21 08:27:26 +0300

    Update delete-my-account.js

--- delete-my-account.js
+++ delete-my-account.js
@@ -1,4 +1,8 @@
-
-var button = document.querySelector("#post-11 > div > div > nav > ul > li.woocommerce-MyAccount-navigation-link.woocommerce-MyAccount-navigation-link--wpf-delete-account > a");
-
-button.onclick = function (e) { e.preventDefault(); confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog."); }
+var parentElement = document.querySelector(".account-overview-profile .card-actions");
+var clonedElement = document.querySelector(".account-overview-profile .card-actions a").cloneNode();
+clonedElement.innerText = "Delete My Account";
+clonedElement.onclick = (e) => {
+    e.preventDefault();
+    confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+};
+parentElement.appendChild(clonedElement);
