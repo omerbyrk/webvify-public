b04eb1323af96853613d64f04fb2653854db2f71
Ömer Bayrak <omersocial8@gmail.com>
2022-11-01 10:55:56 +0300

    a

--- woocommerce-delete_button.js
+++ woocommerce-delete_button.js
@@ -5,6 +5,6 @@ deleteMyButton.onclick = (e) => {
     e.preventDefault();
     confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
 };
-if (parentElement.lastChild.innerText.toUpperCase() !== deleteMyButton.innerText.toLocaleUpperCase()) {
+if (parentElement.lastChild.innerText?.toUpperCase() !== deleteMyButton.innerText?.toUpperCase()) {
     parentElement.appendChild(deleteMyButton);
 }
