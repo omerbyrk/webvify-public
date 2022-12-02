8e3aa565e043b615ec9e063d0b365d65dd05be2a
Ömer Bayrak <omersocial8@gmail.com>
2022-12-02 09:15:52 +0300

    commit

---
+++ saferesell.js
@@ -0,0 +1,14 @@
+
+
+var parentElement = document.querySelector("main > div.page-content.pt-2 > div > div > ul");
+var deleteMyButton = document.createElement("button");
+deleteMyButton.style.marginTop = "10px";
+deleteMyButton.style.marginBottom = "10px";
+deleteMyButton.innerText = "Delete My Account";
+deleteMyButton.onclick = (e) => {
+    e.preventDefault();
+    confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+};
+if (parentElement.lastChild.innerText?.toUpperCase() !== deleteMyButton.innerText?.toUpperCase()) {
+    parentElement.appendChild(deleteMyButton);
+}
