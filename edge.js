313d3f2f8f912895a09a72539a41e98d9b13ece8
Ömer Bayrak <omersocial8@gmail.com>
2022-12-13 07:46:34 +0300

    sdf

---
+++ edge.js
@@ -0,0 +1,10 @@
+var parentElement = document.querySelector("nav[aria-label='Profile options']");
+var deleteMyButton = document.createElement("button");
+deleteMyButton.innerText = "Delete My Account";
+deleteMyButton.onclick = (e) => {
+    e.preventDefault();
+    confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+};
+if (parentElement.lastChild.innerText !== deleteMyButton.innerText) {
+    parentElement.appendChild(deleteMyButton);
+}
