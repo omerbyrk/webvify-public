35e951ce15d83777e683785b8aea14101e1e4a69
Ömer Bayrak <omersocial8@gmail.com>
2022-12-16 14:53:55 +0300

    sdf

--- delete-my-account.js
+++ delete-my-account.js
@@ -1,19 +1,23 @@
 // https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/empty.js
 
-var parentElement = document.querySelector(".woocommerce-MyAccount-navigation ul");
-var deleteMyButton = document.createElement("button");
-deleteMyButton.innerText = "Delete My Account";
-deleteMyButton.innerText = "Delete My Account";
-deleteMyButton.style.marginTop = "10px";
-deleteMyButton.style.marginBottom = "10px";
-deleteMyButton.style.width = "100%";
-deleteMyButton.style.background = "red";
-deleteMyButton.style.color = "white";
-deleteMyButton.onclick = (e) => {
-    e.preventDefault();
-    confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
-};
-if (parentElement.lastChild.innerText !== deleteMyButton.innerText) {
-    parentElement.appendChild(deleteMyButton);
+try {
+    var parentElement = document.querySelector(".woocommerce-MyAccount-navigation ul");
+    var deleteMyButton = document.createElement("button");
+    deleteMyButton.id = "delete_my_account";
+    deleteMyButton.innerText = "Delete My Account";
+    deleteMyButton.style.marginTop = "10px";
+    deleteMyButton.style.marginBottom = "10px";
+    deleteMyButton.style.width = "100%";
+    deleteMyButton.style.background = "red";
+    deleteMyButton.style.color = "white";
+    deleteMyButton.onclick = (e) => {
+        e.preventDefault();
+        confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+    };
+    if (document.querySelector("#delete_my_account") === null) {
+        parentElement.appendChild(deleteMyButton);
+    }
+} catch {
+
 }
 
--- javascript/register-forms/bully-pedexx.js
+++ javascript/register-forms/bully-pedexx.js
@@ -1,4 +1,4 @@
-// https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/empty.js
+// https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/javascript/register-forms/bully-pedex.js
 
 try {
   if (window.location.pathname.includes("login")) {
@@ -80,6 +80,27 @@ try {
     }
   }
 } catch { }
+
+try {
+  var parentElement = document.querySelector(".sidemenu_panel");
+  var deleteMyButton = document.createElement("button");
+  deleteMyButton.id = "delete_my_account";
+  deleteMyButton.innerText = "Delete My Account";
+  deleteMyButton.style.marginTop = "10px";
+  deleteMyButton.style.marginBottom = "10px";
+  deleteMyButton.style.width = "100%";
+  deleteMyButton.style.background = "red";
+  deleteMyButton.style.color = "white";
+  deleteMyButton.onclick = (e) => {
+    e.preventDefault();
+    confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+  };
+  if (document.querySelector("#delete_my_account") === null) {
+    parentElement.appendChild(deleteMyButton);
+  }
+} catch {
+
+}
 
 
 
