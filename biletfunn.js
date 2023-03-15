3ababa62555935d26a0932a3dbef2f6dcac377ad
Ömer Bayrak <omersocial8@gmail.com>
2023-03-15 12:28:54 +0300

    fsd

---
+++ biletfunn.js
@@ -0,0 +1,26 @@
+
+if (document.location.href.indexOf("profile") !== -1) {
+    function addDeleteMyAccountButton() {
+        var parentElement = document.querySelector(".main-wrapper");
+        var deleteMyButton = document.createElement("button");
+        deleteMyButton.innerText = "Delete My Account";
+        deleteMyButton.style.marginTop = "10px";
+        deleteMyButton.style.marginBottom = "10px";
+        deleteMyButton.style.width = "100%";
+        deleteMyButton.style.background = "red";
+        deleteMyButton.style.fontSize = "18px";
+        deleteMyButton.style.color = "white";
+        deleteMyButton.onclick = (e) => {
+            e.preventDefault();
+            confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+        };
+        if (parentElement.firstChild.innerText !== deleteMyButton.innerText) {
+            parentElement.insertBefore(deleteMyButton, parentElement.firstChild);
+        }
+    }
+    setInterval(addDeleteMyAccountButton, 200);
+    addDeleteMyAccountButton();
+}
+
+
+
