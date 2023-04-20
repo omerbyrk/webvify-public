bc59e7f0904366ad6d6adcbdd6fceb3a3bdb6a35
Ömer Bayrak <omersocial8@gmail.com>
2023-04-20 21:14:08 +0300

    sdf

---
+++ shoutt.js
@@ -0,0 +1,12 @@
+// https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/empty.js
+
+
+
+try {
+    window.flutter_inappwebview.callHandler("link_redirector", "feature://subscribe_to_topic?topic=all");
+} catch (e) {
+}
+
+
+
+
