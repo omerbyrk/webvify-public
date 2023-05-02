163440e62901b62dc0b1ba1d07e1357ebb730b1e
Ömer Bayrak <omersocial8@gmail.com>
2023-05-02 19:03:46 +0300

    fsdf

---
+++ meetandmatched.js
@@ -0,0 +1,3 @@
+var style = document.createElement('style');
+style.innerText = "body footer { display: block !important; width: unset !important; height: unset !important; }";
+document.head.appendChild(style);
