c6e0d8949f4785bbf37a730cd6caaba09e6159d6
Ömer Bayrak <omersocial8@gmail.com>
2023-05-03 17:58:30 +0300

    fsdf

--- edubloomss.js
+++ edubloomss.js
@@ -4,27 +4,26 @@
 
 
 
-function addDeleteMyAccountButton() {
+try {
     var parentElement = document.querySelector("#mySidenav");
     var deleteMyButton = document.createElement("button");
+    deleteMyButton.id = "delete_my_account";
     deleteMyButton.innerText = "Delete My Account";
     deleteMyButton.style.marginTop = "10px";
     deleteMyButton.style.marginBottom = "10px";
     deleteMyButton.style.width = "100%";
+    deleteMyButton.style.zIndex = 99;
     deleteMyButton.style.background = "red";
-    deleteMyButton.style.fontSize = "18px";
     deleteMyButton.style.color = "white";
     deleteMyButton.onclick = (e) => {
         e.preventDefault();
         confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
     };
-    if (parentElement.lastChild.innerText !== deleteMyButton.innerText) {
+    if (document.querySelector("#delete_my_account") === null) {
         parentElement.appendChild(deleteMyButton);
     }
+} catch {
+
 }
-setInterval(addDeleteMyAccountButton, 200);
-addDeleteMyAccountButton();
-
-
 
 
