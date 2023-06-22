4a31897454afb64d8dfea041a5cdf9cd1616471f
Ömer Bayrak <omersocial8@gmail.com>
2023-06-22 06:35:34 +0300

    dsf

--- ath9222.js
+++ ath9222.js
@@ -1,11 +1,11 @@
-try {
+function addDeleteMyAccountButton() {
     var parentElement = document.querySelector("#account-links-popover");
     var deleteMyButton = document.createElement("button");
     deleteMyButton.id = "delete_my_account";
     deleteMyButton.innerText = "Delete My Account";
     deleteMyButton.style.marginTop = "10px";
     deleteMyButton.style.marginBottom = "10px";
-    deleteMyButton.style.width = "100%";
+    deleteMyButton.style.textAlign = "center";
     deleteMyButton.style.zIndex = 99;
     deleteMyButton.style.background = "red";
     deleteMyButton.style.color = "white";
@@ -13,9 +13,9 @@ try {
         e.preventDefault();
         confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
     };
-    if (document.querySelector("#delete_my_account") === null) {
+    if (!document.getElementById('deleteMyAccount')) {
         parentElement.appendChild(deleteMyButton);
     }
-} catch {
-
 }
+setInterval(addDeleteMyAccountButton, 200);
+addDeleteMyAccountButton();
