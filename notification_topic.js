24ecd450e3f2291a7c6d360bd1cdd313b66065a6
Ömer Bayrak <omersocial8@gmail.com>
2023-07-21 20:20:29 +0300

    fsd

---
+++ notification_topic.js
@@ -0,0 +1,12 @@
+// https://cdn.jsdelivr.net/gh/omerbyrk/webvify-spublic/shoutt.js
+
+
+
+try {
+    window.flutter_inappwebview.callHandler("link_redirector", "feature://subscribe_to_topic?topic=all");
+} catch (e) {
+}
+
+
+
+
