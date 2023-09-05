e24e75240a49f2429af4c49c2b7961d51f8fab9f
Ömer Bayrak <omersocial8@gmail.com>
2023-09-05 16:50:42 +0300

    sdf

---
+++ tatsup.js
@@ -0,0 +1,29 @@
+// https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/sportsuncle.js
+
+
+
+function addDeleteMyAccountButton() {
+    var parentElement = document.querySelector(".account-header");
+    var deleteMyButton = document.createElement("button");
+    deleteMyButton.innerText = "Delete My Account";
+    deleteMyButton.id = "deleteMyAccount";
+    deleteMyButton.style.marginTop = "10px";
+    deleteMyButton.style.marginBottom = "10px";
+    deleteMyButton.style.width = "100%";
+    deleteMyButton.style.background = "red";
+    deleteMyButton.style.fontSize = "18px";
+    deleteMyButton.style.color = "white";
+    deleteMyButton.onclick = (e) => {
+        e.preventDefault();
+        confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
+    };
+    if (!document.getElementById('deleteMyAccount')) {
+        parentElement.appendChild(deleteMyButton);
+    }
+}
+setInterval(addDeleteMyAccountButton, 2000);
+addDeleteMyAccountButton();
+
+
+
+
