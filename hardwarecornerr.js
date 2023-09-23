f3b98a2b43f7023113c3becc4eb5c8829734bc56
Ömer Bayrak <omersocial8@gmail.com>
2023-09-23 10:20:32 +0300

    sdf

--- hardwarecornerr.js
+++ hardwarecornerr.js
@@ -2,30 +2,27 @@
 
 // https://cdn.jsdelivr.net/gh/omerbyrk/webvify-public/hardwarecorner.js
 
-
-
-
-try {
+function addDeleteMyAccountButton() {
     var parentElement = document.querySelector(".sM3eAi1");
     var deleteMyButton = document.createElement("button");
-    deleteMyButton.id = "delete_my_account";
     deleteMyButton.innerText = "Delete My Account";
+    deleteMyButton.id = "deleteMyAccount";
     deleteMyButton.style.marginTop = "10px";
     deleteMyButton.style.marginBottom = "10px";
     deleteMyButton.style.width = "100%";
-    deleteMyButton.style.zIndex = 99;
     deleteMyButton.style.background = "red";
+    deleteMyButton.style.fontSize = "18px";
     deleteMyButton.style.color = "white";
     deleteMyButton.onclick = (e) => {
         e.preventDefault();
         confirm("Do you want to delete the account with all the stored data? It will be deleted if you confirm this dialog. ")
     };
-    if (document.querySelector("#delete_my_account") === null) {
+    if (!document.getElementById('deleteMyAccount')) {
         parentElement.appendChild(deleteMyButton);
     }
-} catch {
-
 }
+setInterval(addDeleteMyAccountButton, 200);
+addDeleteMyAccountButton();
 
 
 
