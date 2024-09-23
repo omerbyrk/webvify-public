f39d125c4e0e28cca48a4fa543fa760f87705447
Ömer Bayrak <omersocial8@gmail.com>
2024-09-23 20:07:08 +0300

    sdf

---
+++ smartshop.js
@@ -0,0 +1,3 @@
+if (location.href == "https://d9c6c9-2.myshopify.com" || location.href == "https://d9c6c9-2.myshopify.com/") {
+    location.href = "https://d9c6c9-2.myshopify.com/?refresh=true";
+}
