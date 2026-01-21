8eec91d41f1ed77e53e401ad02084905a72895bb
Ömer Bayrak <omer.bayrak06@hotmail.com>
2026-01-21 19:11:35 +0300

    asdf

---
+++ dameso.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    try {
+        document.querySelector(".Compliance-cs-accept-btn").click();
+        document.querySelector(".ecomsend-SpinWheel__Modal__CloseText").click()
+
+    } catch (e) { }
+}, 50);
