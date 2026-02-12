4acaac71795f53c50b450b44e6063aa66eb6aeed
Ömer Bayrak <omer.bayrak06@hotmail.com>
2026-02-12 20:47:19 +0300

    asdf

--- dameso.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-    try {
-        document.querySelector(".Compliance-cs-accept-btn").click();
-        document.querySelector(".ecomsend-SpinWheel__Modal__CloseText").click()
-
-    } catch (e) { }
-}, 50);
